e95487ff24da94e06708d4e056462864a8087c41
GMurit <fjavierhuelvacontact@gmail.com>
2025-12-11 18:48:20 +0100

    En vez de hacer las conexiones a base de datos con orientación a objetos, las voy a hacer de modo normal que es como las estoy haciendo ahora en clase de cliente

--- aplicacion/configuracion/config.php
+++ aplicacion/configuracion/config.php
@@ -6,10 +6,10 @@ $contrasena = "";
 $base_datos = "cliniko";
 
 //GUARDAMOS EN ESTA VARIABLE LA PROPIA CONEXIÓN CON LOS DATOS DE ARRIBA.
-$conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
+$conexion = mysqli_connect($host, $usuario, $contrasena, $base_datos);
 
 //VERIFICAMOS LA CONEXIÓN Y EN CASO DE QUE HAYA UN ERROR, LO MOSTRAMOS.
-if ($conexion->connect_error) {
-    die("La conexión no se puede realizar: " . $conexion->connect_error);
+if (!$conexion) {
+    die("La conexión no se puede realizar: " . mysqli_connect_error());
 }
 ?>
